e985640f2081a14df07a79bbeba4467be17c4d71
Hui (Tom) Tang <htang2013@gmail.com>
2019-04-12 10:00:58 -0400

    Delete dupRm.py

--- dupRm.py
+++
@@ -1,8 +0,0 @@
-### Remove adjacent repeated characters in each string of a list ###
-# use itertools
-
-import itertools
-
-def dupRm(s_list):
-
-	return([''.join(ch for ch, _ in itertools.groupby(s)) for s in s_list])	
